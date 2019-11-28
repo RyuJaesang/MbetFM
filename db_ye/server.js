2ffcc7514a08806ac4e923541c4724da2d4ed5c3
Yeongeun Kang <39408863+yeonun@users.noreply.github.com>
2019-11-29 01:07:15 +0900

    데이터 빼고 다 완료
    끗

--- db_ye/server.js
+++ db_ye/server.js
@@ -20,11 +20,28 @@ app.get('/', function(req, res){
     var first = req.query.first
     var second = req.query.second
     var third = req.query.third
-    var sql='SELECT * FROM music WHERE genre_1=? AND genre_2=? AND genre_3=?'
+    var sql='SELECT * FROM performance WHERE genre_1=? AND genre_2=? AND genre_3=?'
     var params=[first,second,third]
     connection.query(sql, params,function(err, rows, fields) {
         if (err) res.send('err : ' + err)
-        if (rows.length > 0 ) console.log('The 결과 is: ', rows[0].id);
-    res.send(first+','+second+','+third)
+        if (rows.length > 0 ) {
+          console.log('The 결과 is: ', rows[0].performance_genre)
+
+          var sql2='SELECT * FROM exhibition WHERE genre_1=? AND genre_2=? AND genre_3=?'
+          var params2=[first,second,third]
+          connection.query(sql2, params2,function(err2, rows2, fields2) {
+            if (err) {
+              res.send('err : ' + err)
+              return
+            }
+            if (rows.length > 0 ) {
+              res.send('casual/ workwear/ street'+','+rows[0].performance_genre+','+rows2[0].exhibitions_genre)
+            }else{
+              res.send('서버에러','결과가','엄서용')
+            }
+          });
+        }else{
+          res.send('서버에러','결과가','엄서용')
+        }
     });
 });
